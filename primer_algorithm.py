b2232978ef6633bf62a32ab25d138beef2026df7
Johan Lehto <93596859+SvartKaffe@users.noreply.github.com>
2022-05-05 16:17:33 +0200

    added some primer conditions

--- primer_algorithm.py
+++ primer_algorithm.py
@@ -6,7 +6,7 @@ from Bio import SeqIO
 # random comment
 
 
-def find_primers(sequence, delta_t):
+def find_primers(sequence, temp):
     i = 0
     primers = []
     while i <= len(sequence):
@@ -14,30 +14,47 @@ def find_primers(sequence, delta_t):
         primer = ""
         for base in sequence[i:]:
             if base == "A":
-                if sum + 2 <= delta_t:
+                if sum + 2 <= temp:
                     sum += 2
                     primer = primer + base
                 else:
                     break
             elif base == "T":
-                if sum + 2 <= delta_t:
+                if sum + 2 <= temp:
                     sum += 2
                     primer = primer + base
                 else:
                     break
             elif base == "G":
-                if sum + 4 <= delta_t:
+                if sum + 4 <= temp:
                     sum += 4
                     primer = primer + base
                 else:
                     break
             elif base == "C":
-                if sum + 4 <= delta_t:
+                if sum + 4 <= temp:
                     sum += 4
                     primer = primer + base
                 else:
                     break
-        if primer not in primers:
+
+        primer_length = len(primer)
+        g_count = primer.count("G")
+        c_count = primer.count("C")
+        if g_count > 0 and c_count > 0:
+            gc_ratio = (g_count + c_count) / primer_length
+        else:
+            gc_ratio = 0
+
+        primer_conditions = (
+                primer_length > temp//4
+                and primer not in primers
+                and (primer[-1] == "G"
+                or primer[-1] == "C")
+                and (0.4 <= gc_ratio <= 0.6)
+        )
+
+        if primer_conditions:
             primers.append(primer)
         i += 1
     return primers
@@ -47,5 +64,5 @@ if __name__ == "__main__":
     for sequence1 in SeqIO.parse("Enterobacteria-phage-P2-NC_001895-complete-genome.fasta", "fasta"):
         sequence2 = sequence1.seq
     test = "ATGTCAATGGCTATCGTCACTATT"
-    a = find_primers(sequence2, 10)
+    a = find_primers(sequence2, 60)
     print(a)
