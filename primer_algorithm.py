0b5f3ba65b18b3d997be7a338becaf36c40034a9
Johan Lehto <93596859+SvartKaffe@users.noreply.github.com>
2022-05-09 18:28:31 +0200

    should remove duplicate primers that fulfill primer conditions

--- primer_algorithm.py
+++ primer_algorithm.py
@@ -6,6 +6,7 @@ import re
 def find_primers(sequence, temp, reverse=False):
     i = 0
     primers = {}
+    duplicate_primers = []
     pattern = re.compile("(C|G)(C|G)(C|G)")
     while i <= len(sequence):
         sum = 0
@@ -36,8 +37,8 @@ def find_primers(sequence, temp, reverse=False):
                 else:
                     break
 
+        # Primer computations and conditions
         primer_length = len(primer)
-
         if reverse:
             start = len(sequence) - i
             stop = start + 1 - primer_length
@@ -57,28 +58,40 @@ def find_primers(sequence, temp, reverse=False):
 
         primer_conditions = (
                 primer_length > temp//4
-                and primer not in primers.keys()
-                and (primer[-1] == "G"
-                or primer[-1] == "C")
+                and (primer[-1] == "G" or primer[-1] == "C")
                 and (0.4 <= gc_ratio <= 0.6)
                 and not m
         )
 
         if primer_conditions:
-            primers[primer] = {"length": primer_length,
-                               "start": start,
-                               "stop": stop,
-                               "temp": sum
-                               }
+            if (primer in primers.keys()) and (primer not in duplicate_primers):
+                duplicate_primers.append(primer)
+            else:
+                primers[primer] = {"length": primer_length,
+                                   "start": start,
+                                   "stop": stop,
+                                   "temp": sum
+                                   }
+
         i += 1
-    return primers
+    # Removes any primers found multiple times in the sequence
+    for item in duplicate_primers:
+        if item in primers.keys():
+            primers.pop(item)
+
+    return primers, duplicate_primers
+
 
 def unique_primers(sequence, primers, delta_t):
     print()
 
+
 if __name__ == "__main__":
     for sequence1 in SeqIO.parse("Enterobacteria-phage-P2-NC_001895-complete-genome.fasta", "fasta"):
         sequence2 = sequence1.seq
-    test = "ATGTCAATGGCTATCGTCACTATT"
-    a = find_primers(sequence2, 60)
+    test = "ATGTCAATGGCTATCGTCACTATTGTACTTAGCCAATTAG"
+    a,b = find_primers(sequence2, 60)
     print(len(a))
+    print('ACCAACAGCACGTCTGAAAC' in a.keys())
+
+
